47933d3520634c3633c5f4a368c63b25807b4403
Aditya-KNV <knvaditya06@gmail.com>
2026-09-03 20:30:23 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,33 @@
+class MinStack {
+
+private:
+    stack<pair<int, int>> st;
+
+public:
+    MinStack() {
+
+    }
+    
+    void push(int val) {
+        if(st.empty()) {
+            st.push({val, val});
+        }
+        else {
+            int mini = st.top().second;
+            if(mini > val) mini = val;
+            st.push({val, mini});
+        }
+    }
+    
+    void pop() {
+        st.pop();
+    }
+    
+    int top() {
+        return st.top().first;
+    }
+    
+    int getMin() {
+        return st.top().second;
+    }
+};
